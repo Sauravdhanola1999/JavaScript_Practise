659490499b0b46475c45b26573d3d4e461bffd88
Saurav Singh Dhanola <125140898+Sauravdhanola1999@users.noreply.github.com>
2024-09-25 20:59:30 +0530

    Add files via upload

---
+++ day3.js
@@ -0,0 +1,74 @@
+// iterate array through loops
+
+// const array = ["banana", "apple", "something", "lost"];
+// for(let i=0; i<array.length; i++){
+//     console.log(array[i]);
+// }
+
+//print number from 1 to 15
+
+
+// for(let i=1; i<=15; i++){
+//     console.log(i);
+// }
+
+// odd number 7 to 15
+
+// for(let i=7; i<=15; i++){
+//     if(i%2!=0){
+//         console.log(i);
+//     }
+// }
+
+// 
+
+// #4 — Print the EVEN numbers from 10 to -20
+
+// for(let i=10; i>=-20; i--){
+//     if(i%2===0){
+//         console.log(i);
+//     }
+// }
+
+// Iterate through all numbers from 1 to 45. Print the following:
+
+// For multiples of 3 print “Fizz”
+// For multiples of 5 print “Buzz”
+// For multiples of 3 and 5 print “FizzBuzz”
+
+// for(let i=1; i<=45; i++){
+//     if(i%3===0 && i%5===0){
+//         console.log("FizzBuzz", i);
+//     }
+//     else if(i%3===0){
+//         console.log("Fizz", i);
+//     }
+//     else if(i%5===0){
+//         console.log("Buzz", i);
+//     }
+// }
+
+
+// #7 — Calculate the sum of all the numbers in the following array
+
+
+// let numbersArray = [1,13,22,123,49]
+// let sum=0;
+
+// for(let i=0; i<numbersArray.length; i++){
+//     sum= sum + numbersArray[i];
+// }
+// console.log("Sum of Arrays", + sum);
+
+// check leap year
+
+let year = 1900;
+
+if(year%100===0){
+    console.log("it is not a leap year")
+    }
+    else {
+        console.log("it is a leap year")
+    }
+
+
